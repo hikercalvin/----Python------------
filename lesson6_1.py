efac7265269be7ecbca1456354e92bb103b595d6
Aster Wei <tristan416@gmail.com>
2022-11-16 15:24:31 +0800

    modify

--- lesson6_1.py
+++ lesson6_1.py
@@ -1,6 +1,14 @@
-def sayHello():
+def sayHello(): #沒有參數的function
     print("Hello!")
 
+def sayHello_withName(name): #有參數的function
+    print(name)
+
+def square_area(side):
+    area = side ** 2 #function內的變數
+    return area 
+
 if __name__ == "__main__":
-    sayHello()
-    
+    side = eval(input("請輸入正方形的邊:"))
+    area = square_area(side) #文件變數
+    print(f"正方形，一邊為{side},面積為{area}")
